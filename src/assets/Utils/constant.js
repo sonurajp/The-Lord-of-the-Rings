ea10186243ad2d67425825df91b9e1a7bafdc853
Sonuraj P <sonuraj@litmus7.com>
2023-11-28 01:44:55 +0530

    add constant

---
+++ src/assets/Utils/constant.js
@@ -0,0 +1,21 @@
+export const limit = [
+  { label: 20, value: 20 },
+  { label: 10, value: 10 },
+  { label: 50, value: 50 },
+];
+export const sort = [
+  { label: "Ascending", value: "asc" },
+  { label: "Descending", value: "desc" },
+];
+export const race = [
+  { label: "Any", value: "" },
+  { label: "Hobbit", value: "Hobbit" },
+  { label: "Human", value: "Human" },
+  { label: "Orc", value: "Orc" },
+];
+
+export const gender = [
+  { label: "Any", value: "" },
+  { label: "Male", value: "Male" },
+  { label: "Female", value: "Female" },
+];
